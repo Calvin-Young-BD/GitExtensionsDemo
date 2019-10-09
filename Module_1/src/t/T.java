9caca2ac5dd0f8623c7021024c2454883bfb069e
Calvin <CalvinBD@163.com>
2019-10-09 10:53:37 +0800

    添加T.java的内容

--- Module_1/src/t/T.java
+++ Module_1/src/t/T.java
@@ -3,5 +3,6 @@ package t;
 public class T {
     public static void main(String[] args) {
         System.out.println("The first time to use IDEA push project to Git!");
+        System.out.println("The first time to use IDEA push project to Git!");
     }
 }
